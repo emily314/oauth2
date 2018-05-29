d421b2004747bcb4290c50a9a79ec30117b29f7b
zhyanyan <zhyanyan@cn.ibm.com>
2018-05-29 14:34:54 +0800

    deploy to cloud

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const oauthClientId = exports.oauthClientId = 'nyq9a331f51gxyk5ozt1izqoklbj7587'
 const oauthClientSecret = exports.oauthClientSecret = 'FmBjk1fCwWBJDJFMmkW3MixplIfqjcFs';
 
 // Redirect URI
-const redirectURI = 'http://localhost:5100/return';
+const redirectURI = 'https://enigmatic-thicket-40662.herokuapp.com/return';
